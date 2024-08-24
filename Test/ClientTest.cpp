e430b37ec76adfe6ef0636c26a9719edf70c4378
podo3501 <nixie77@naver.com>
2024-08-25 01:44:37 +0900

    mock으로 클라이언트 부분 테스트

--- Test/ClientTest.cpp
+++ Test/ClientTest.cpp
@@ -1,38 +1,69 @@
 #include "gmock/gmock.h"
 #include "../Include/NetworkInterface.h"
+#include "SerializationTestClass.h"
 
 namespace Client
 {
-    class MockTest {
+    class FakeTCPClient : public TCPClient
+    {
     public:
-        MOCK_METHOD(void, SomeMethod, ());
+        virtual bool Connect(const std::string& serverAddress) { return true; }
+        virtual bool Send(const void* data, size_t len, int32_t* recvBytes) { return true; }
+        virtual bool Receive(void* data, size_t len, int32_t* recvBytes) override;
+        virtual bool Shutdown(int shutdownFlag = SD_SEND) { return true; }
     };
 
-    TEST(TestCaseName, TestName) {
-        MockTest mock;
-        EXPECT_CALL(mock, SomeMethod);
-        mock.SomeMethod();
-        EXPECT_EQ(1, 1);
-        EXPECT_TRUE(true);
+    bool FakeTCPClient::Receive(void* data, size_t len, int32_t* recvBytes)
+    {
+        CMSTest toMS(12, 3.5f, 0, 0, { 3.5, 4.5 }, 3);
+        OutputMemoryStream writeStream;
+        toMS.Write(writeStream);
+
+        size_t realLength = min(len, writeStream.GetBufferSize());
+        std::copy(writeStream.GetBufferPtr(), writeStream.GetBufferPtr() + realLength, reinterpret_cast<std::uint8_t*>(data));
+        if (recvBytes != nullptr) (*recvBytes) = static_cast<int32_t>(writeStream.GetBufferSize());
+
+        return true; 
     }
 
     class MockTCPClient : public TCPClient
     {
     public:
+        FakeTCPClient fake;
+
+        MockTCPClient() { DelegateToFake(); }
+
         MOCK_METHOD(bool, Connect, (const std::string& serverAddress), (override));
         MOCK_METHOD(bool, Send, (const void* data, size_t len, int32_t* recvBytes), (override));
         MOCK_METHOD(bool, Receive, (void* data, size_t len, int32_t* recvBytes), (override));
-        //MOCK_METHOD(bool, Shutdown, (int shutdownFlag), (override));
-        MOCK_METHOD1(Shutdown, bool(int shutdownFlag));
+        MOCK_METHOD(bool, Shutdown, (int shutdownFlag), (override));
+
+    private:
+        void DelegateToFake()
+        {
+            using ::testing::_;
+            ON_CALL(*this, Receive(_, _, _)).WillByDefault(::testing::Invoke(&fake, &FakeTCPClient::Receive));
+        }
     };
 
-    using ::testing::_;
     TEST(MockTCPSocket, Test)
     {
-        std::unique_ptr<MockTCPClient> mockTcpClient = std::make_unique<MockTCPClient>();
-        EXPECT_CALL(*mockTcpClient, Connect(_)).Times(1);
-        //EXPECT_CALL(*mockTcpClient, Shutdown(_)).Times(1);
-        mockTcpClient->Connect("");
-        //mockTcpClient->Shutdown();
+        MockTCPClient mockTcpClient;
+        EXPECT_CALL(mockTcpClient, Connect(testing::_)).Times(1);
+        EXPECT_CALL(mockTcpClient, Shutdown(testing::_)).Times(1);
+
+        mockTcpClient.Connect("");
+
+        InputMemoryStream readStream(4096);
+        int32_t recvBytes = 0;
+        mockTcpClient.Receive(readStream.GetBufferPtr(), 4096, &recvBytes);
+
+        mockTcpClient.Shutdown(SD_SEND);
+
+        CMSTest fromMS;
+        fromMS.Read(readStream);
+
+        CMSTest expectMS(12, 3.5f, 0, 0, { 3.5, 4.5 }, 3);
+        EXPECT_EQ(fromMS, expectMS);
     }
 }
